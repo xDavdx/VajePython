9f78d28a1f26613490dbbb8fea17664104d5827b
AGregorc <anze.gregorc44@gmail.com>
2018-03-25 16:22:00 +0200

    Dodani komentarji za zadnjo nalogo

--- analyseFollowers.py
+++ analyseFollowers.py
@@ -1,21 +1,30 @@
 from bs4 import BeautifulSoup
 import codecs
 
+# Odpre datoteke z načinom r -> read
 fileing = codecs.open("following.html", 'r', 'utf-8')
 fileers = codecs.open("followers.html", 'r', 'utf-8')
 
+# Prebere obe datoteki (vse kar je v datoteki zapisanega)
 followingHTML = fileing.read()
 followersHTML = fileers.read()
 
+# Inicializira razreda tipa BeautifulSoup, 1. argument je string (ki vsebuje html kodo), 2. pa pove, da je string tipa html
 souping = BeautifulSoup(followingHTML, 'html.parser')
 soupers = BeautifulSoup(followersHTML, 'html.parser')
 
+# Nov seznam (array) po imenu following
 following = []
+# Najdi vse html objekte, ki vsebujejo class "_2g7d5 notranslate _o5iw8" in ga vsakič daj v spremenljivko one
 for one in souping.find_all("a", attrs={"class": "_2g7d5 notranslate _o5iw8"}):
+	# Doda v seznam (array) novega najdenega userja
     following.append(one.string)
 
+# Nov seznam (array) po imenu followers
 followers = []
+# Najdi vse html objekte, ki vsebujejo class "_2g7d5 notranslate _o5iw8" in ga vsakič daj v spremenljivko one
 for one in soupers.find_all("a", attrs={"class": "_2g7d5 notranslate _o5iw8"}):
+	# Doda v seznam (array) novega najdenega userja
     followers.append(one.string)
 
 # Naloga: 
